de46b15b843ba08782a068b97700045e0a4f901e
Claudio168 <claudiogonzalez0712@gmail.com>
2021-03-15 00:31:24 -0300

    Proyecto en produccion 4

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  path: path.resolve(__dirname, '/.vuepress/dist/index.html'),
+  base: '/dist/index.html',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },//Home es lo que se pinta en el navbar 
